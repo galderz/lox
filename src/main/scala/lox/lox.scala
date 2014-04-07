28b3a7f05c987a23a77c449f5a2107cc3c705b02
Galder Zamarreño <galder@zamarreno.com>
2014-04-07 18:46:52 +0200

    A simpler base script for running all commands

--- src/main/scala/lox/lox.scala
+++ src/main/scala/lox/lox.scala
@@ -3,154 +3,49 @@ SCALAZ_JAR=/Users/g/.ivy2/cache/org.scalaz/scalaz-core_2.11.0-RC3/jars/scalaz-co
 exec scala -classpath $SCALAZ_JAR -savecompiled -feature "$0" "$@"
 !#
 
-import java.io.{IOException, PrintStream, InputStream}
-import scala.collection.mutable.ListBuffer
-import scalaz.{~>,Id,Free,Functor}, Free.Return, Free.Suspend, Id.Id
-import scala.language.implicitConversions
+import scala.language.higherKinds
+import scalaz._
+import Scalaz._
 
-import Environment.env
+sealed trait Action[+N]
 
-val cmdargs = args.toList
+type Args = Seq[String]
 
-cmdargs match {
-  case Nil =>
-    println("Run Lox test suite")
+object Action {
+  case class Clean[N](args: Args, next: N) extends Action[N]
+  case object Done extends Action[Nothing]
 
-  case x::xs =>
-    x match {
-      case "clear" =>
-        val program: Free[EnvironmentF, Unit] =
-          for {
-            a <- env.clean(Array(""))
-          } yield a
+  def clean(args: Args): Free[Action, Unit] = liftF[Action, Unit](Clean(args, ()))
+  def done(): Free[Action, Unit] = liftF[Action, Unit](Done)
 
-        Maven(program, DummySystem)
-        println(DummySystem())
+  implicit val actionToFunctor: Functor[Action] = new Functor[Action] {
+    def map[A, B](fa: Action[A])(f: A => B): Action[B] = fa match {
+      case c: Clean[A] => Clean(c.args, f(c.next))
+      case Done => Done
     }
-}
-
-//val program: Free[ProcessF, Unit] =
-//  for {
-//    a <-
-//  } yield a
-
-// Basic tests
-//OperativeSystem(process.execute(Array("java", "-version")))
-// Maven(env.clean(Array("")))
-
-// Supporting classes - Environment
-
-type Recipe = Array[String]
-type Instructions = Seq[String]
-
-sealed trait EnvironmentF[+A]
-
-object Environment {
-  type Environment[A] = Free[EnvironmentF, A]
-
-  implicit def envFFunctor[B]: Functor[EnvironmentF] = new Functor[EnvironmentF]{
-    def map[A,B](fa: EnvironmentF[A])(f: A => B): EnvironmentF[B] =
-      fa match {
-        case Clean(recipe,a) => Clean(recipe,f(a))
-        case Build(recipe,a) => Build(recipe,f(a))
-        case Test(recipe,a) => Test(recipe,f(a))
-      }
   }
 
-  implicit def envFToFree[A](logf: EnvironmentF[A]): Free[EnvironmentF,A] =
-    Suspend[EnvironmentF, A](Functor[EnvironmentF].map(logf)(a => Return[EnvironmentF, A](a)))
-
-  case class Clean[A](recipe: Recipe, o: A) extends EnvironmentF[A]
-  case class Build[A](recipe: Recipe, o: A) extends EnvironmentF[A]
-  case class Test[A](recipe: Recipe, o: A) extends EnvironmentF[A]
-
-  object env {
-    def clean(recipe: Recipe): Environment[Unit] = Clean(recipe, ())
-    def build(recipe: Recipe): Environment[Unit] = Build(recipe, ())
-    def test(recipe: Recipe): Environment[Unit] = Test(recipe, ())
-  }
-
+  private def liftF[F[+_]: Functor, R](command: F[R]): Free[F, R] =
+    Free.Suspend[F, R](Functor[F].map(command) { Free.Return[F, R](_) })
 }
 
 object Maven {
-  import Environment._
-
-  var process: Process = _
-
-  private def execute(recipe: Recipe): Unit = process.execute(recipe)
-  private def clean(recipe: Recipe): Unit = execute(Array("mvn", "clean"))
-  private def build(recipe: Recipe): Unit = execute(Array("mvn", "-Dmaven.test.skip.exec=true", "install"))
-  private def test(recipe: Recipe): Unit = execute(Array("mvn", "install"))
-
-  private val exe: EnvironmentF ~> Id = new (EnvironmentF ~> Id) {
-    def apply[B](l: EnvironmentF[B]): B = l match {
-      case Clean(recipe,a) => { clean(recipe); a }
-      case Build(recipe,a) => { build(recipe); a }
-      case Test(recipe,a) => { test(recipe); a }
-    }
-  }
-
-  def apply[A](log: Environment[A], process: Process): A = {
-    this.process = process
-    log.runM(exe.apply[Environment[A]])
-  }
-
+  import Action._
+  def showProgram[R: Show](p: Free[Action, R]): String =
+    p.resume.fold({
+      case Clean(a, next) =>
+        "mvn clean " + a.mkString(" ") + "\n" + showProgram(next)
+      case Done => ""
+    },
+    { r: R => "return " + Show[R].shows(r) + "\n" })
 }
 
-// Supporting classes - Process execution
+// Script
 
-trait Process {
-  def execute(recipe: Recipe): Unit
-}
+import Action._
 
-object DummySystem extends Process {
-  private var executed = new ListBuffer[String]
-  override def execute(recipe: Recipe): Unit = executed += recipe.mkString(" ")
-  def apply(): Instructions = executed.toList
-}
-
-object OperativeSystem extends Process {
-  override def execute(recipe: Recipe): Unit = {
-      val builder = new ProcessBuilder(recipe : _*)
-      builder.redirectErrorStream(true)
-      val process = builder.start()
-      val stdout = process.getInputStream
-      val consoleConsumer = new ConsoleConsumer(stdout, java.lang.System.out)
-      val outThread = new Thread(consoleConsumer, "System")
-      outThread.start()
-
-      try {
-        process.waitFor()
-      } catch {
-        case _: InterruptedException => process.destroy()
-      }
-
-      // This ensures that all output is complete
-      // before returning (waitFor does not ensure this)
-      outThread.join()
-
-      if (process.exitValue() != 0)
-        throw new Exception(s"[$recipe] execution failed in ${builder.directory()}")
-    }
-
-    private class ConsoleConsumer(source: InputStream, target: PrintStream) extends Runnable {
-      def run() {
-        val source = this.source
-        try {
-          val buf = new Array[Byte](32)
-          // Do not try reading a line cos it considers '\r' end of line
-          var reading = true
-          while (reading) {
-            source.read(buf) match {
-              case -1 => reading = false
-              case c => target.write(buf, 0, c)
-            }
-          }
-        } catch {
-          case e: IOException => e.printStackTrace(target)
-        } finally {
-          source.close()
-        }
-      }
-    }
-}
+val program = for {
+  _ <- clean(List())
+  _ <- done()
+} yield ()
+print(Maven.showProgram(program))
